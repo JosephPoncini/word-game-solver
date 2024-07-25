cc7f76ca010f2afb7a1f8996ee62bb4dbb100fd0
JosephPoncini <jxponcini@gmail.com>
2024-07-25 12:23:45 -0700

    try deploying

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -72,7 +72,7 @@ export const getWordHuntData = async (board:string[][]) => {
     };
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/api/word_hunt_get_data', {
+        const response = await fetch('https://poncini-wordle-solver.azurewebsites.net/api/word_hunt_get_data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
